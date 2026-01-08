db532c6946433dac3b56ba6d26135dac23e9e423
Johannes Kiewath <j.kiewath@web.de>
2026-01-08 15:12:28 -0500

    Clean up repository (ignore build and VS Code history files)

--- .history/src/main_20260108104458.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "HexEditor – Phase 2 gestartet\n";
-    return 0;
-}
--- .history/src/main_20260108110646.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "HexEditor - Phase 2 gestartet\n";
-    return 0;
-}
